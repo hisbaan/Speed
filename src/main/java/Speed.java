11ba52529cda3bdbdfc63bf57a778b1e866f9fd7
Hisbaan <34548959+hisbaan@users.noreply.github.com>
2019-08-08 11:43:48 -0500

    made the project

---
+++ src/main/java/Speed.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Speed {
+    public static void main(String[] args) {
+        new Speed();
+    }
+
+    Speed() {
+
+    }
+}
